356ba7afccb9f83e3481273b1da624a048c9d70c
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2017-10-03 21:09:12 +0900

    modify page_controller.ex

--- lib/supervisor_phoenix_web/controllers/page_controller.ex
+++ lib/supervisor_phoenix_web/controllers/page_controller.ex
@@ -1,7 +1,19 @@
 defmodule SupervisorPhoenixWeb.PageController do
-  use SupervisorPhoenixWeb, :controller
+	use SupervisorPhoenixWeb, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
+	def index(conn, _params) do
+		params = SupervisorPhoenix.Counter.get_state
+		call_inc(SupervisorPhoenix.Counter)
+		render(conn, "index.html", params)
+	end
+
+	defp call_inc(process_name) do
+		process_name.inc
+	end
+
+	def kill(conn, _) do
+		SupervisorPhoenix.Counter.kill()
+		url = page_path(conn, :index)
+		redirect(conn, to: url)
+	end
 end
