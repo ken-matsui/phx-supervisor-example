c779195f59b343fbf6cf5d8f5cfa9a05e70c121e
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2017-10-03 21:08:44 +0900

    add routing

--- lib/supervisor_phoenix_web/router.ex
+++ lib/supervisor_phoenix_web/router.ex
@@ -17,6 +17,7 @@ defmodule SupervisorPhoenixWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/kill", PageController, :kill
   end
 
   # Other scopes may use custom stacks.
